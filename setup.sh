0fd49b843a652bfc9cfc7b02c3842cd1e5fe938f
Eirik Vinje <eirik.matias@gmail.com>
2024-10-12 19:55:40 +0200

    Save

--- setup.sh
+++ setup.sh
@@ -6,29 +6,27 @@ if [ "$(basename "$PWD")" != "saliencyserieslab" ]; then
     exit 1
 fi
 
-# check if the path "data/insectsound" exists and exit if it does
-if [ -d "data/insectsound" ]; then
-    echo "setup is already done"
-    exit 1
+DATA_PATH="$(pwd)/data/insectsound"
+if [ ! -d "$DATA_PATH" ]; then
+    cd data
+    echo "Downloading the InsectSound dataset..."
+    wget https://www.timeseriesclassification.com/aeon-toolkit/InsectSound.zip
+    unzip InsectSound.zip
+    mkdir -p data/insectsound
+    mv InsectSound/* data/insectsound
+    rm -rf InsectSound.zip
+    rm -rf InsectSound
+    cd ..
 fi
 
-echo "Setting up environment..."
-mkdir results
-mkdir models
-mkdir plots
-mkdir data
-mkdir log
-
-echo "Downloading the InsectSound dataset..."
-
-cd data
-wget https://www.timeseriesclassification.com/aeon-toolkit/InsectSound.zip
-unzip InsectSound.zip
-mkdir insectsound
-mv InsectSound/* insectsound
-rm -rf InsectSound.zip
-rm -rf InsectSound
-
-echo "Installing dependencies..."
+LOG_PATH="$(pwd)/log"
+if [ ! -d "$LOG_PATH" ]; then
+    echo "Setting up environment..."
+    mkdir results
+    mkdir models
+    mkdir plots
+    mkdir data
+    mkdir log
+fi
 
 pip install -r requirements.txt
