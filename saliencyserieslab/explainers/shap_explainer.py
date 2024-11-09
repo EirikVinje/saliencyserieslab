cb0325678eec7155136dc881922e24f9c74d2ce9
Eirik Vinje <eirik.matias@gmail.com>
2024-11-09 16:09:12 +0100

    save

--- saliencyserieslab/explainers/shap_explainer.py
+++ saliencyserieslab/explainers/shap_explainer.py
@@ -7,8 +7,9 @@ import os
 import numpy as np
 import shap
 
+from saliencyserieslab.plotting import plot_simple_weighted
 from saliencyserieslab.classifier import SktimeClassifier
-from saliencyserieslab.plotting import plot_weighted_graph
+from aeon.datasets import load_classification
 
 
 class KernelShapExplainer:
@@ -57,4 +58,55 @@ class GradientShapExplainer:
 
 if __name__ == "__main__":
     
-    pass
+    np.random.seed(42)
+
+    modelpath = "./models/rocket_ECG200_1"
+    dataset = modelpath.split("/")[-1].split("_")[1]
+
+    model = SktimeClassifier()
+    model.load_pretrained_model(modelpath)
+
+    test = load_classification(dataset, split="test")
+
+    unique_classes = np.unique(test[1]).tolist()
+    unique_classes = [int(c) for c in unique_classes]
+    unique_classes.sort()
+ 
+    test = {
+        "x" : test[0].squeeze(),
+        "y" : np.array([unique_classes.index(int(c)) for c in test[1]]),
+    }
+
+    method = "kernel"
+
+    if method == "kernel":
+
+        explainer = KernelShapExplainer(
+            model=model,
+            x_background=shap.sample(test["x"], 1),
+        )
+
+    elif method == "grad":
+
+        explainer = GradientShapExplainer(
+            model=model,
+            x_background=shap.sample(test["x"], 1)
+        )
+
+    print("loaded model and explainer : ({}, {})".format(model.__class__.__name__, explainer.__class__.__name__))
+    
+    sample = test["x"][np.random.randint(0, test["x"].shape[0])]
+
+    w = explainer.explain_instance(sample)
+
+    sample = sample.reshape(-1)
+    w = w.reshape(-1)
+
+    plot_simple_weighted(
+        sample, 
+        w, 
+        f"./plots/{method}shap_{datetime.datetime.now().strftime('%Y%m%d%H%M%S')}.png",
+        model.model.__class__.__name__,
+        explainer.__class__.__name__,
+        dataset
+        )
